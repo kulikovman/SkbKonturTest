92d0c5ce2073d02f77fb456317b2348a147e9182
Maksim Kulikov <maxim.kulikov@osinit.com>
2019-06-15 20:45:12 +0500

    Добавление простого поиска в акшенбар

--- app/src/main/java/ru/kulikovman/skbkonturtest/SearchFragment.java
+++ app/src/main/java/ru/kulikovman/skbkonturtest/SearchFragment.java
@@ -1,5 +1,6 @@
 package ru.kulikovman.skbkonturtest;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -9,6 +10,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -27,5 +31,37 @@ public class SearchFragment extends Fragment {
         mViewModel = ViewModelProviders.of(this).get(ContactViewModel.class);
         // TODO: Use the ViewModel
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Подключаем макет меню
+        inflater.inflate(R.menu.search_menu, menu);
+
+        // Обработчик поисковых запросов
+        MenuItem searchItem = menu.findItem(R.id.search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String searchQuery) {
+                /*preparingForLoadPhoto(searchQuery);
+
+                // Получение фото
+                showProgressBar();
+                loadSearchPhoto(mSearchQuery, 1, true);
+
+                // Свертывание поиска
+                searchView.clearFocus();
+                searchView.onActionViewCollapsed();*/
+
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
 
 }
